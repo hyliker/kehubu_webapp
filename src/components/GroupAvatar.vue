7cac708eda2ba71723a1c90f673d533781df3bbb
hyliker <hyliker@gmail.com>
2019-04-15 23:53:34 +0800

    Tweak GroupDetailQRcode

---
+++ src/components/GroupAvatar.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <img :src="group.logo" v-if="group.logo" :width="size" :height="size" />
+  <van-icon name="circle" :size="size" v-else />
+</div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: '50px',
+    },
+    group: Object,
+  }
+}
+</script>
--- src/components/GroupDetailQRcode.vue
+++ src/components/GroupDetailQRcode.vue
@@ -3,28 +3,63 @@
     <van-nav-bar
   title="群二维码名片"
   left-text="返回"
-  @click-left="onClickLeft"
+  @click-left="$router.go(-1)"
   />
-   <qrcode class="qrcode" tag="img" :value="link" v-if="link"></qrcode>
+  <div class="content">
+  <van-cell>
+    <div class="meta">
+      <group-avatar :group="group" size="50px" class="avatar" />
+      <span class="name">{{ group.name }}</span>
+    </div>
+    <qrcode class="qrcode" tag="img" :value="link" :options="{margin: 0}" v-if="link"></qrcode>
+    <p class="description">用微信扫一扫上面的二维码图案，加入群组</p>
+    </van-cell>
+  </div>
   </div>
 </template>
 
 <style>
+.content {
+  margin: 5px;
+}
+.name {
+  float: left;
+  margin-left: 10px;
+  margin-top: 10px;
+  font-size: 1.5em;
+}
+.description {
+  text-align: center;
+  margin-top: 20px;
+}
+.avatar {
+  float: left;
+}
 .qrcode {
   width: 100%;
-  height: 100%;
+  margin-top: 5px;
 }
 
 </style>
 
 
 <script>
+import GroupAvatar from '@/components/GroupAvatar.vue';
+import {mapState} from 'vuex';
 export default {
   props: ['id'],
+  components: {
+    GroupAvatar,
+  },
   data: function () {
     return {
       link: '',
     }
+  },
+  computed:{
+    ...mapState({
+      group: state => state.currentGroup,
+    })
   },
   methods: {
     onClickLeft () {
