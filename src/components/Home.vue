84c5b7b74133b64d80838d50356213710f61e536
hyliker <hyliker@gmail.com>
2019-04-16 14:31:13 +0800

    Tweak Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
 <div class="home">
     <img class="logo" alt="客户簿-沟通客户好助手" src="@/assets/img/kehubu-slogan@3x.png">
     <br />
-    <img alt="微信扫码登录" src="@/assets/img/login_300px.png" />
+    <img alt="微信扫码登录" class="login" src="@/assets/img/login_300px.png" />
     <p>微信扫一扫， 快速注册</p>
 </div>
 </template>
@@ -14,6 +14,11 @@
 .logo {
   width: 300px;
   margin: 5px 0px;
+}
+.login {
+  margin-top: 10px;
+  border: 1px dashed #777;
+  background-color:#fff;
 }
 </style>
 
